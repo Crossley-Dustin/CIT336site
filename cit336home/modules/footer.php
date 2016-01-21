eb4ae8852de65220d738b5a139d10b6b4a4c7bed
crossley-dustin <kuark7@hotmail.com>
2016-01-20 23:48:51 -0800

    Added CIT230 presentation for reference on the site from the footer

--- cit336home/modules/footer.php
+++ cit336home/modules/footer.php
@@ -2,6 +2,7 @@
             <nav>
                 <ul>
                     <li><a href="/cit336home/exercises.php" title="Exercises">Exercises</a></li>
+                    <li><a href="/cit336home/presentation_230/presentation.php" title="CIT 230 Presentation" target="_blank">CIT 230 Presentation</a></li>
                     <!--<li><a href="/assessments/deliberate-practice.php" title="View the Deliberate Practice Plan">Practice Plan</a></li>-->
                     <!--<li><a href="http://www.presentation.sandboxthought.com/index.php" title="View Presentation">Presentation</a></li>-->
                 </ul>
---
+++ cit336home/presentation/presentation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dustin's Teaching Presentation | PHP Includes</title>
+        <script type="text/javascript" src="/cit336home/presentation_230/assets/pdfobject.js"></script>
+        <script type="text/javascript">
+        window.onload = function(){
+            var myPDF = new PDFObject({ url: "/cit336home/presentation_230/assets/teachingpresentation_phpincludes.pdf"}).embed();
+        };
+        </script>
+    </head>
+    <body>
+        <p>It appears you don't have Adobe Reader or PDF support in this web
+            browser. <a href="/assets/teachingpresentation_phpincludes.pdf">Click here to download the PDF presentation</a></p>
+    </body>
+</html>
